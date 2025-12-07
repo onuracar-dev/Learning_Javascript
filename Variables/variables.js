3078259e0e32a767656758012d4e40b9e4268138
Onur Acar <onuracar.work@gmail.com>
2025-12-07 12:11:02 +0300

    Variables

---
+++ Variables/variables.js
@@ -0,0 +1,19 @@
+// let age = 25;
+// let price = 10.99;
+
+// console.log(`Your age is ${age}`);
+// console.log(`Your price is ${price}`)
+
+// let forSale = true;
+// let isStudent = true;
+
+// console.log(`Is this car for sale ${forSale}`);
+// console.log(`Enrolled ${isStudent}`);
+
+let fullName = `Onur`;
+let age = 20;
+let enrolled = false;
+
+document.getElementById(`p1`).textContent = `Your name is ${fullName}`;
+document.getElementById(`p2`).textContent = `Your age is ${age}`;
+document.getElementById(`p3`).textContent = `Enrolled ${enrolled}`;
