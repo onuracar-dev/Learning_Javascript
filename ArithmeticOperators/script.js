ae1d1dda29dd303268ca01ba6e062db7f289a838
Onur Acar <onuracar.work@gmail.com>
2025-12-14 15:47:03 +0300

    ArithmeticOperators

---
+++ ArithmeticOperators/script.js
@@ -0,0 +1,20 @@
+let students = 30;
+
+students = students + 1;
+students = students - 1;
+students = students * 1;
+students = students / 1;
+students = students ** 1;
+let ExtraStudents = students % 1;
+
+students += 1;
+students -= 1;
+students *= 1;
+students /= 1;
+students **= 1;
+students %= 1;
+
+students++;
+students--;
+
+console.log(students);
