36cc2c2110be06096070f3a32d342295fab1581e
Onur Acar <onuracar.work@gmail.com>
2025-12-16 13:07:35 +0300

    MathObject

---
+++ MathObject/script.js
@@ -0,0 +1,20 @@
+let x = 3;
+let y = 2;
+let z = 1;
+
+z = math.round(x);
+z = math.floor(x); //round down
+z = math.ceil(x);  //round up
+z = math.trunc(x); //eliminate decimal
+z = math.pow(x,y); // power of
+z = math.sqrt(x);; 
+z = math.log(x);
+z = math.sin(x);
+z = math.cos(x);
+z = math.tan(x);
+z = math.abs(x); //absolute
+z = math.sign(x);
+let max = math.max(x,y,z);
+let min = math.min(x,y,z);
+
+console.log(min)
