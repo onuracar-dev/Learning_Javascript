e381a884c529a73dd4824087760e91feb6fa8a15
Onur Acar <onuracar.work@gmail.com>
2025-12-18 16:57:05 +0300

    StringMethods

---
+++ StringMethods/script.js
@@ -0,0 +1,15 @@
+let userName = `Onur`
+
+userName.charAt(2);
+userName.indexOf(`n`);
+userName.lastIndexOf(`u`);
+userName.length;
+userName = userName.trim //its remove the space
+userName.toUpperCase;
+userName.toLowerCase;
+userName.repeat(2); // it will repeat 2 times
+userName.startsWith(`O`); // true if its start with O if isnt its false
+userName.endsWith(`O`); // same but in the end
+userName.includes(` `); // check all word
+userName.replaceAll(`n`,`d`); // change
+userName.padStart(10, `O`); // add O until userName 10 if you use .padEnd O is add to end
