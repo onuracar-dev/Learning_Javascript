b1424f2eada8f1a0c68b234f7bb7187b398b4b84
Onur Acar <onuracar.work@gmail.com>
2025-12-24 12:49:58 +0300

    MethodChaining

---
+++ MethodChaining/script.js
@@ -0,0 +1,20 @@
+let username = window.prompt(`Enter your username`);
+
+// ------ NO METHOD CHAINING ------ //
+
+// username = username.trim();
+// let letter = username.charAt(0);
+// letter = letter.toUpperCase();
+
+// let extraChars = username.slice(1);
+// extraChars = extraChars.toLowerCase();
+// username = letter + extraChars;
+
+// console.log(username);
+
+
+// ------ METHOD CHAINING --------- //
+
+username = username.trim().charAt(0).toUpperCase() + username.trim().slice(1).toLowerCase();
+
+console.log(username);
