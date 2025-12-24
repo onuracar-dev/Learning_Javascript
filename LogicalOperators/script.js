336af48d0727bbf8ad33e275dd975b4a86ed4d0b
Onur Acar <onuracar.work@gmail.com>
2025-12-24 13:08:20 +0300

    LogicalOperators

---
+++ LogicalOperators/script.js
@@ -0,0 +1,24 @@
+let deg = 20;
+
+if(deg <= 0 && deg >= 30) {
+    console.log(`Weather is bad`)
+}
+else {
+    console.log(`Weather is good`)
+}
+
+if(deg <=0 || deg >= 30) {
+    console.log(`Weather is bad`)
+}
+else {
+    console.log(`Weather is good`)
+}
+
+let sunny = true;
+
+if(!sunny) {
+    console.log(`It is cloudy`)
+}
+else {
+    console.log(`It is sunny`)
+}
